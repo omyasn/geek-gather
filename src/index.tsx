8c2c07b3c73e93557d3128483c9f6f50f8eb55a5
Olga Myasn <omyasn91@gmail.com>
2021-04-21 14:57:21 +0300

    add watcher

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,5 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./App.js";
 
-console.log('!!!!!CLIENT!!!!!!!!!!!!!!!!!!');
+console.log('!!!!!CLIENT!!!!!!!!!!!!!!');
 ReactDOM.hydrate(<App />, document.getElementById("root"));
