a39315595b5a9f0cc120c99f83fbf5e3d7a2590d
OlgaMyasnikova <myasnikova@manychat.com>
2022-05-08 23:47:23 +0400

    new fake data

---
+++ src/mockData/events.ts
@@ -0,0 +1,226 @@
+const events = [
+  {
+    id: "evnt1",
+    title: "BPM: Brigades",
+    beginDate: "2022-05-23",
+    location: {
+      name: "Sanatorium 'Green Town'",
+      city: "Moscow",
+      link: "https://google.com", // TODO location page
+    },
+    capacity: 2000,
+    parties: "2",
+    minPrice: 2000,
+    owners: [
+      {
+        name: "APG",
+        link: "https://google.com", // TODO owner link
+      },
+      {
+        name: "Caliber",
+        link: "https://google.com", // TODO location page
+      },
+    ],
+    private: false,
+    hasQuests: true,
+    ownBalls: false,
+  },
+  {
+    id: "evnt2",
+    title: "Mercenaries2: Operation Flow",
+    beginDate: "2022-05-29",
+    location: {
+      name: "Polygon 'Star'",
+      city: "Moscow",
+      link: "https://google.com", // TODO location page
+    },
+    capacity: 300,
+    parties: "1+1 vs 1+1",
+    minPrice: 1500,
+    owners: [
+      {
+        name: "Restricted Area",
+        link: "https://google.com", // TODO owner link
+      },
+    ],
+    private: false,
+    hasQuests: true,
+    ownBalls: true,
+  },
+  {
+    id: "evnt3",
+    title: "VETERANS: Banner of Victory",
+    beginDate: "2022-06-19",
+    location: {
+      name: "Polygon Noginsk-2",
+      city: "Noginsk",
+      link: "https://google.com", // TODO location page
+    },
+    capacity: 1000,
+    parties: "2",
+    minPrice: 900,
+    owners: [
+      {
+        name: "Real Games",
+        link: "https://google.com", // TODO owner link
+      },
+    ],
+    private: false,
+    hasQuests: false,
+    ownBalls: true,
+  },
+  {
+    id: "evnt4",
+    title: "Afganistan",
+    beginDate: "2022-07-01",
+    location: {
+      name: "Polygon 'Star'",
+      city: "Moscow",
+      link: "https://google.com", // TODO location page
+    },
+    capacity: 400,
+    parties: "2",
+    minPrice: 800,
+    owners: [
+      {
+        name: "Restricted Area",
+        link: "https://google.com", // TODO owner link
+      },
+    ],
+    private: false,
+    hasQuests: false,
+    ownBalls: true,
+  },
+  {
+    id: "evnt5",
+    title: "FALLOUT XIII",
+    beginDate: "2022-07-21",
+    location: {
+      name: "Polygon 'RSB'",
+      city: "Moscow",
+      link: "https://google.com", // TODO location page
+    },
+    capacity: 600,
+    parties: "3",
+    minPrice: 1000,
+    owners: [
+      {
+        name: "Restricted Area",
+        link: "https://google.com", // TODO owner link
+      },
+    ],
+    private: false,
+    hasQuests: false,
+    ownBalls: true,
+  },
+  {
+    id: "evnt6",
+    title: "Stories of Old Man",
+    beginDate: "2022-07-30",
+    location: {
+      name: "Raining Forest Club",
+      city: "St.Peterbugr",
+      link: "https://google.com", // TODO location page
+    },
+    capacity: 300,
+    parties: "2",
+    minPrice: 1000,
+    owners: [
+      {
+        name: "Legendarium",
+        link: "https://google.com", // TODO owner link
+      },
+    ],
+    private: false,
+    hasQuests: true,
+    ownBalls: false,
+  },
+  {
+    id: "evnt7",
+    title: "Weekend Game",
+    beginDate: "2022-06-30",
+    location: {
+      name: "Bunker Club",
+      city: "Moscow",
+      link: "https://google.com", // TODO location page
+    },
+    capacity: 20,
+    parties: "2",
+    minPrice: 600,
+    owners: [
+      {
+        name: "Andrew",
+        link: "https://google.com", // TODO USER link
+      },
+    ],
+    private: true,
+    hasQuests: false,
+    ownBalls: false,
+  },
+  {
+    id: "evnt7",
+    title: "Training",
+    beginDate: "2022-07-07",
+    location: {
+      name: "Bunker Club",
+      city: "Moscow",
+      link: "https://google.com", // TODO location page
+    },
+    capacity: 20,
+    parties: "2",
+    minPrice: 500,
+    owners: [
+      {
+        name: "Deadlyfang",
+        link: "https://google.com", // TODO USER link
+      },
+    ],
+    private: true,
+    hasQuests: false,
+    ownBalls: false,
+  },
+  {
+    id: "evnt8",
+    title: "Big Bears War",
+    beginDate: "2022-08-07",
+    location: {
+      name: "Raining Forest Club",
+      city: "St.Peterbugr",
+      link: "https://google.com", // TODO location page
+    },
+    capacity: 400,
+    parties: "2",
+    minPrice: 1200,
+    owners: [
+      {
+        name: "It Is What It Is",
+        link: "https://google.com", // TODO USER link
+      },
+    ],
+    private: false,
+    hasQuests: true,
+    ownBalls: true,
+  },
+  {
+    id: "evnt9",
+    title: "Game for all",
+    beginDate: "2022-08-10",
+    location: {
+      name: "Centrum Club",
+      city: "Noginsk",
+      link: "https://google.com", // TODO location page
+    },
+    capacity: 30,
+    parties: "2",
+    minPrice: 550,
+    owners: [
+      {
+        name: "Peter",
+        link: "https://google.com", // TODO USER link
+      },
+    ],
+    private: true,
+    hasQuests: false,
+    ownBalls: false,
+  },
+];
